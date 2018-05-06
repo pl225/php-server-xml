e567aaeb8ea3a93c99c5e060b1878e9745ae0c40
Matheus Abreu <matheusa596@gmail.com>
2018-05-06 14:37:49 -0300

    Última alteração no retorno para xmls mal formados e inválidos.

--- server.php
+++ server.php
@@ -8,7 +8,7 @@
 	function customError($errno, $errstr, $errfile, $errline, $errcontext) { // para mandar o código de erro interno
 		
 		if (strpos($errno, 'DOMDocument::loadXML()') !== false && strpos($errno, 'DOMDocument::schemaValidate()') !== false) { // se ocorreu erro em algum lugar do código q nao sejam os erros dos xmls, lança erro interno
-	  		echo str_replace('VALOR', '3', str_replace('METODO', 'erro', $errcontext['retorno'])); // o último parâmetro serve para pegar as variáveis declaradas no escopo de onde aconteceu o erro (simplesmente para nao copiar a variavel de retorno para dentro da funcao, ela nao eh visivel aqui )
+	  		echo str_replace('VALOR', '3', $errcontext['retorno']); // o último parâmetro serve para pegar as variáveis declaradas no escopo de onde aconteceu o erro (simplesmente para nao copiar a variavel de retorno para dentro da funcao, ela nao eh visivel aqui )
 	  	}
 	}
 
@@ -24,9 +24,12 @@
 	$submeter = 'submeter';
 
 	$req = null;
+
+	$retorno = str_replace('METODO', $_SERVER['REQUEST_METHOD'] == 'GET' ? $consultarStatus : $submeter, $retorno);
+	$valorErro = $_SERVER['REQUEST_METHOD'] == 'GET' ? ['-1', '-2'] : ['1', '2'];
 	
 	if (key($_GET))
-		$req = key($_GET); 
+		$req = key($_GET);
 	else if (key($_POST))
 		$req = key($_POST);
 	else
@@ -39,23 +42,23 @@
 				
 			if ($xml->getElementsByTagName("methodName")[0]->nodeValue == $consultarStatus) {
 				
-				$cpf = $xml->getElementsByTagName("cpf")[0]->nodeValue;
-				echo str_replace('VALOR', substr($cpf, -1), str_replace('METODO', $consultarStatus, $retorno)); // os cpfs sempre terao o ultimo caractere igual ao codigo q temos q retornar, entao eh so pegar o ultimo caractere da string
+				$cpf = substr($xml->getElementsByTagName("cpf")[0]->nodeValue, -1);
+				echo str_replace('VALOR', $cpf >= '0' && $cpf <= '4' ? $cpf : '4', $retorno); // os cpfs sempre terao o ultimo caractere igual ao codigo q temos q retornar, entao eh so pegar o ultimo caractere da string
 			
 			} else if ($xml->getElementsByTagName("methodName")[0]->nodeValue == $submeter) { // envia 0 em caso de sucesso
       			
-      			echo str_replace('VALOR', '0', str_replace('METODO', $submeter, $retorno));
+      			echo str_replace('VALOR', '0', $retorno);
       		
       		}
 
 		} else { // se o esquema estiver errado envia o codigo de erro
 			
-			echo str_replace('VALOR', '1', str_replace('METODO', 'erro', $retorno));
+			echo str_replace('VALOR', $valorErro[0], $retorno);
 		
 		}  
   	} else { // se o xml estiver mal formatado envia o codigo de erro
     	
-    	echo str_replace('VALOR', '2', str_replace('METODO', 'erro', $retorno));
+    	echo str_replace('VALOR', $valorErro[1], $retorno);
   	
   	}  
 
